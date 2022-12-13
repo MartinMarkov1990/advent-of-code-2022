90add1ba28f9423ce583fccdf066d97f0f612299
Martin Markov <martin.markov1990@gmail.com>
2022-12-13 17:41:46 +0200

    Day 6 part 2 added'

--- day_6/communicator.py
+++ day_6/communicator.py
@@ -11,25 +11,23 @@ def read_in_chunks(file_object, chunk_size=1024):
         yield data
 
 
-def process_chunk(chunk: str, last_three: deque):
+def process_chunk(chunk: str, buffer: deque, buffer_size=4):
     for no, char in enumerate(chunk):
-        print(f"{no+1}: {char}")
-        last_three.append(char)
-        if len(last_three) == 4 and len(set(last_three)) == 4:
-            return no + 1, ''.join(last_three)
-        elif len(last_three) == 4:
-            last_three.popleft()
-        print(''.join(last_three))
+        buffer.append(char)
+        if len(buffer) == buffer_size and len(set(buffer)) == buffer_size:
+            return no + 1, ''.join(buffer)
+        elif len(buffer) == buffer_size:
+            buffer.popleft()
     return None, ''
 
 
 if __name__ == '__main__':
     chunk_size = 1024
     chunk_no = 0
-    last_three = deque([])
+    buffer = deque([])
     with open('input.txt', 'r') as f:
         for chunk in read_in_chunks(f, chunk_size=chunk_size):
-            index, str_sequence = process_chunk(chunk, last_three)
+            index, str_sequence = process_chunk(chunk, buffer, buffer_size=14)
             if index:
                 print(f'Index number {index + chunk_no*chunk_size}, unique sequence {str_sequence}')
                 break
