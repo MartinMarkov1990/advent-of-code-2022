eddbf767813a01982ba60cd241293bbc6ed536c9
Martin Markov <martin.markov1990@gmail.com>
2022-12-14 18:58:14 +0200

    Day 7 part 1

---
+++ day_7/parse_directory_tree.py
@@ -0,0 +1,81 @@
+import re
+from enum import Enum, auto
+
+from anytree import AnyNode, Resolver, ChildResolverError, LevelOrderIter
+
+
+class DirTreeCommandsParseError(BaseException):
+    pass
+
+
+class Dir(AnyNode):
+    @property
+    def size(self):
+        if len(self.children) == 0:
+            return 0
+        return sum([child.size for child in self.children])
+
+
+class File(AnyNode):
+    def __init__(self, size, *args, **kwargs):
+        AnyNode.__init__(self, *args, **kwargs)
+        self.size = size
+
+
+class ParseState(Enum):
+    CD = auto()
+    LS = auto()
+
+
+class FileTree:
+    def __init__(self, filename='input.txt'):
+        self.filename = 'input.txt'
+        self.root = None
+        self.current = None
+        self._state = None
+        self.resolver = Resolver()
+
+    def get_child_dir(self, dir: str) -> AnyNode():
+        try:
+            child_dir = self.resolver.get(self.current, dir)
+        except ChildResolverError:
+            child_dir = Dir(name=dir, parent=self.current)
+        return child_dir
+
+    def parse_file(self):
+        for line in open(self.filename, 'r'):
+            if line[-1] == '\n':
+                line = line[0:-1]
+            if line.startswith('$ cd'):
+                self._state = ParseState.CD
+                dir = line[5:]
+                if dir == '..':
+                    self.current = self.current.parent
+                else:
+                    if self.root is None:
+                        self.root = Dir(name=dir)
+                        self.current = self.root
+                    else:
+                        self.current = self.get_child_dir(dir)
+            elif line == '$ ls':
+                self._state = ParseState.LS
+            elif line.startswith('dir'):
+                dir = line[4:]
+                self.get_child_dir(dir)
+            else:
+                m = re.match('([0-9]+)\\s+([a-zA-Z].*)', line)
+                if not m:
+                    raise DirTreeCommandsParseError(f'Unknown command format: {line}')
+                size, filename = m.groups()
+                File(size=int(size), name=filename, parent=self.current)
+
+
+if __name__ == '__main__':
+    THRESHOLD = 100000
+    filetree = FileTree()
+    filetree.parse_file()
+    small_dirs = [node for node in LevelOrderIter(filetree.root) if isinstance(node, Dir) and node.size <= THRESHOLD]
+    for dir in small_dirs:
+        print(f"{dir.path}: {dir.size}")
+    total_size_small_dirs = sum([dir.size for dir in small_dirs])
+    print(f"Total size to save: {total_size_small_dirs}")
